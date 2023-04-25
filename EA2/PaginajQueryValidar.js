d445bde87d46f72bf29f04993e2300ee5170db6c
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-25 13:43:15 -0400

    update

--- EA2/PaginajQueryValidar.js
+++ EA2/PaginajQueryValidar.js
@@ -1,5 +1,35 @@
 $(function()
 {
+    let numeros = '1234567890';
+    let letras  = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNM ';
+    
+    $('.txtRut').keypress(function(e)
+    {
+        // obtener el caracter presionado por el usuario
+        let caracter = String.fromCharCode(e.which);
+        if(numeros.indexOf(caracter) < 0)
+            return false;
+    })
+
+    $('.txtDv').keypress(function(e)
+    {
+        let patron = numeros + 'kK';
+        let caracter = String.fromCharCode(e.which);
+        if(patron.indexOf(caracter) < 0)
+            return false;
+    })
+
+    $('.btnLimpiar').click(function()
+    {
+/*        $('.txtRut').val('');
+        $('.txDv').val('');
+        $('.txtNombre').val('');5
+        $('.txtEmail').val('');
+ */       
+        
+        $('.txtRut, .txtDv, .txtNombre, .txtEmail').val('');
+        $('.txtRut').focus();
+    });
     $('.btnAceptar').click(function()
     {
         if(!$.trim($('.txtRut').val()))
