c250fc07911dec0545ecbcc1befb511c3a768f6f
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-05-30 12:02:26 -0400

    update

--- EA3/proyecto002/instituto/crud/models.py
+++ EA3/proyecto002/instituto/crud/models.py
@@ -26,3 +26,23 @@ class Categoria(models.Model):
         return self.nombre
     
 
+class Genero(models.Model):
+    idGenero= models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, null=True, unique=True)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    idCliente= models.AutoField(primary_key=True)
+    rut = models.IntegerField(unique=True, null=False, blank=False)
+    dv = models.CharField(max_length=1,null=False, blank=False)
+    nombre = models.CharField(max_length=50, null=False, unique=False)
+    apellido = models.CharField(max_length=50, null=False, unique=False)
+    telefono = models.CharField(max_length=50, null=False, unique=False)
+    email = models.EmailField(max_length=50, null=False, unique=False)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
