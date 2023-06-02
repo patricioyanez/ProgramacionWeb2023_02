4a7810a3551eec12859c100964c8a79f794d5da5
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-02 12:50:22 -0400

    update

--- EA3/proyecto002/instituto/crud/views.py
+++ EA3/proyecto002/instituto/crud/views.py
@@ -6,9 +6,9 @@ def marca(request):
     #orm
     context = {}
     if request.method == 'POST':
-        id = request.POST['id']
+        id = int("0" + request.POST['id'])
         nombre = request.POST['nombre']
-        activo = request.POST['activo']
+        activo = 'activo' in request.POST
 
         if 'Grabar' in request.POST: # fue presionado el grabar
             if id == 0: # insert
@@ -26,5 +26,18 @@ def marca(request):
         elif 'Listar' in request.POST:
             listado = Marca.objects.all() # select * from marca
             context = {'listado': listado}
-    
+        elif 'Buscar' in request.POST:
+            try:
+                item = Marca.objects.get(pk = id)
+                context = {'item': item }
+            except:
+                context = {'error' : 'El id no fue encontrado'}
+        elif 'Eliminar' in request.POST:
+            try:
+                item = Marca.objects.get(pk = id)
+                item.delete()
+                context = {'exito' : 'El id fue ELIMINADO'}
+            except:
+                context = {'error' : 'El id no fue encontrado'}
+
     return render(request, 'marca.html', context)
